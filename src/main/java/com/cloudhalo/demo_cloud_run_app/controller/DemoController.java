3a3e2ddbee5d0b65849214bfd1e6ac400e5cfe4a
Srija shenkeshi <srijashenkeshi@gmail.com>
2024-07-21 20:18:50 +0530

    added pubsub

--- src/main/java/com/cloudhalo/demo_cloud_run_app/controller/DemoController.java
+++ src/main/java/com/cloudhalo/demo_cloud_run_app/controller/DemoController.java
@@ -4,10 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import static com.cloudhalo.demo_cloud_run_app.pubsub.PublisherExample.publisherExample;
+import static com.cloudhalo.demo_cloud_run_app.pubsub.SubscribeAsyncExample.subscribeAsyncExample;
+
 @RestController
 public class DemoController {
 
     @GetMapping("/")
+    public void testTopic() throws IOException, ExecutionException, InterruptedException {
+        publisherExample("srija-my-project", "my-topic");
+        //subscribeAsyncExample("srija-my-project", "my-sub");
+    }
+
+
+    @GetMapping("/welcome")
     public String welcomeMsg(){
         return "Hi this is Srija's App";
     }
